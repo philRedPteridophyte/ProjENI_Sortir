c437a0e1a684c284c3a7a25a2d094d2bb68cfe92
Damien Leneveu <dleneveu22@gmail.com>
2021-03-22 15:44:41 +0100

    feat(2014): adding a Ville is now possible

--- src/Controller/VilleController.php
+++ src/Controller/VilleController.php
@@ -6,6 +6,7 @@ use App\Entity\Inscription;
 use App\Entity\Sortie;
 use App\Entity\Ville;
 use App\Form\LieuxSearchType;
+use App\Form\VilleAddType;
 use App\Form\VilleEditType;
 use App\Form\VilleSearchType;
 use Doctrine\ORM\EntityManager;
@@ -69,6 +70,32 @@ class VilleController extends AbstractController
             'user' => $user,
             'villeEditForm' => $villeEditForm->createView(),
             'villeToEdit' => $villeToEdit
+        ]);
+    }
+
+    #[Route('/ville/add', name: 'addVille')]
+    public function addVille(Request $request, EntityManagerInterface $em): Response
+    {
+        $user = $request->getSession()->get('compteConnecte');
+
+        $villeAddForm = $this->createForm(VilleAddType::class, new Ville());
+        $villeAddForm->handleRequest($request);
+
+        $villeToAdd = new Ville();
+
+        if ($villeAddForm->isSubmitted() && $villeAddForm->isValid()) {
+            if ($villeAddForm->get('submit')->isClicked()) {
+                $villeToAdd->setNomVille($villeAddForm->get('nomVille')->getData());
+                $villeToAdd->setCodePostal($villeAddForm->get('codePostal')->getData());
+                $em->persist($villeToAdd);
+                $em->flush();
+                return $this->redirectToRoute('ville', $request->query->all());
+            }
+        }
+
+        return $this->render('ville/addVille.html.twig', [
+            'user' => $user,
+            'villeAddForm' => $villeAddForm->createView(),
         ]);
     }
 }
