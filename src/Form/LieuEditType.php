c0da41028f044e33fe1b43ee2bd3bfc4b5b40ac3
Germain François <francois.germain2019@campus-eni.fr>
2021-03-25 15:03:55 +0100

    gestion des lieux

---
+++ src/Form/LieuEditType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Lieu;
+use App\Entity\Ville;
+use App\Repository\LieuxRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LieuEditType extends AbstractType
+{
+    public function __construct(LieuxRepository $em)
+    {
+        $this->em = $em;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nomLieu')
+            ->add('rue')
+            ->add('latitude')
+            ->add('longitude')
+            ->add('ville', EntityType::class, [
+                'label' => "Ville",
+                'class' => Ville::class,
+                'choice_label' => 'nomVille'
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Modifier',
+                'attr' => [
+                    'class' => 'btn btn-success'
+                ]
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Lieu::class,
+        ]);
+    }
+}
--- src/Repository/LieuxRepository.php
+++ src/Repository/LieuxRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Lieu;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Query;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -17,6 +18,15 @@ class LieuxRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Lieu::class);
+    }
+
+    public function findAllThatContains($text): ?Array
+    {
+        return $this->createQueryBuilder('o')
+            ->andWhere('o.nomLieu LIKE :nomLieu')
+            ->setParameter('nomLieu', '%'.$text.'%')
+            ->getQuery()
+            ->getResult(Query::HYDRATE_ARRAY);
     }
 
     // /**
