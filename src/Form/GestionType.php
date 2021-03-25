4b4b1917909fb4e84c250d716f387ae6ce89a627
Damien Leneveu <dleneveu22@gmail.com>
2021-03-25 11:52:05 +0100

    data consistency in modif profil form is ok

--- src/Form/GestionType.php
+++ src/Form/GestionType.php
@@ -9,6 +9,7 @@ use App\Repository\LieuxRepository;
 use App\Repository\SiteRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\ResetType;
@@ -16,6 +17,7 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class GestionType extends AbstractType
 {
@@ -39,21 +41,30 @@ class GestionType extends AbstractType
         $builder
             ->add('pseudo', TextType::class, [
                 'label' => 'Pseudo',
-                'required' => false
+                'constraints' => [
+                        new Regex('/^[\w]+$/')
+                ],
+                'required' => true
             ])
             ->add('prenom', TextType::class, [
                 'label' => 'Prénom',
-                'required' => false
+                'constraints' => [
+                    new Regex('/^\pL+$/u')
+                ],
+                'required' => true
             ])
             ->add('nom', TextType::class, [
                 'label' => 'Nom',
-                'required' => false
+                'constraints' => [
+                    new Regex('/^\pL+$/u')
+                ],
+                'required' => true
             ])
             ->add('telephone', TextType::class, [
                 'label' => 'Téléphone',
                 'required' => false
             ])
-            ->add('mail', TextType::class, [
+            ->add('mail', EmailType::class, [
                 'label' => 'Mail',
                 'required' => false
             ])
